f9d1250776b734c5e8f3b547b9c184512cd72711
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 13:12:33 +0100

    Basis commit

--- src/main/java/com/github/hanyaeger/tutorial/TankArtillery.java
+++ src/main/java/com/github/hanyaeger/tutorial/TankArtillery.java
@@ -1,6 +1,19 @@
 package com.github.hanyaeger.tutorial;
 
-public class TankArtillery {
-    System.out.print("test");
-    TestTestTest
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+
+public class TankArtillery extends YaegerGame {
+    public static void main(String[] args){launch(args);}
+
+    @Override
+    public void setupGame() {
+        setGameTitle("Tankartillery");
+        setSize(new Size(800, 600));
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
 }
